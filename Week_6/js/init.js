f72d0768c5faea2a353b003439095ddb293c231d
rachan2023 <rachan2023@g.ucla.edu>
2021-05-08 19:54:29 -0700

    Finished and submitting lab 5

---
+++ Week_6/js/init.js
@@ -0,0 +1,74 @@
+const map = L.map('map').setView([37.7749, -122.4194], 10);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+let url = "https://spreadsheets.google.com/feeds/list/1ErJfY74kCqb2yvCt_MzLyJUvB-tNF5JD9TwxnCybTJs/o9c8k0l/public/values?alt=json"
+fetch(url)
+	.then(response => {
+		return response.json();
+		})
+    .then(data =>{
+        console.log(data)
+        formatData(data)
+    })
+
+function addMarker(data){
+        L.marker([data.lat,data.lng]).addTo(map).bindPopup(`<h2>${data.whereisyourfavoritetraveldestination}</h2> <p>${"Number of times visited: " + data.howmanytimeshaveyouvisitedyourfavoritetraveldestination} </p> <p> ${"Best season to visit: " + data.whatseasonisthebesttimetovisityourfavoritetraveldestination} </p> <h4> ${"Date Posted: " + data.timestamp} </h4>`)
+        createButtons(data.lat,data.lng,data.whereisyourfavoritetraveldestination);
+
+        return data   
+}
+
+function createButtons(lat,lng,whereisyourfavoritetraveldestination){
+    const newButton = document.createElement("button"); // adds a new button
+    newButton.id = "button"+whereisyourfavoritetraveldestination; // gives the button a unique id
+    newButton.innerHTML = whereisyourfavoritetraveldestination; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng]); //this is the flyTo from Leaflet
+    })
+    const spaceForButtons = document.getElementById('contents')
+    spaceForButtons.appendChild(newButton);//this adds the button to our page.
+}
+// addMarker(37,-122,'home land!')
+// let myArray = ['hello','this','is','an','array']
+// myArray.forEach(justChecking);
+// function justChecking(data){
+//     console.log(data)
+// }
+
+// let grocery = ['orange', 'apple', 'berry']
+// grocery.forEach(inBag);
+// function inBag(data) {
+//     console.log(data + ' is in grocery bag.')
+// }
+
+fetch(url)
+	.then(response => {
+		return response.json();
+		})
+    .then(data =>{
+                // console.log(data)
+                formatData(data)
+        }
+)
+
+function formatData(theData){
+        const formattedData = [] /* this array will eventually be populated with the contents of the spreadsheet's rows */
+        const rows = theData.feed.entry
+        for(const row of rows) {
+          const formattedRow = {}
+          for(const key in row) {
+            if(key.startsWith("gsx$")) {
+                  formattedRow[key.replace("gsx$", "")] = row[key].$t
+            }
+          }
+          formattedData.push(formattedRow)
+        }
+        console.log(formattedData)
+        formattedData.forEach(addMarker)        
+}
+
